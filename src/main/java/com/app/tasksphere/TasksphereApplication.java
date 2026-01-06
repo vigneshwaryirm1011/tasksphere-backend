4ec2344a052e400d2d0791dd68c6f60cf571f09c
VigneshwaryiRM <blue.design.arts@gmail.com>
2026-01-06 23:08:12 +0000

    Initial project setup with User entity

---
+++ src/main/java/com/app/tasksphere/TasksphereApplication.java
@@ -0,0 +1,13 @@
+package com.app.tasksphere;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TasksphereApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TasksphereApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/app/tasksphere/TasksphereApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.tasksphere;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TasksphereApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
